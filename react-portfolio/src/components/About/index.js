2a0441a4da84ad62d79bfbf6ef2dbe7fe19475fe
Phanimal <jameslphan@hotmail.com>
2022-12-24 02:04:09 -0800

    components

---
+++ react-portfolio/src/components/About/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+    return(
+        <div>
+            <h1>Greetings, my friends call me James but I go by,... James!</h1>
+            <p>I currently work at Boeing as an assembler/mechanic for the 777 Flight Deck systems installation while studying to become a full stack
+                developer through the UW Bootcamp program. Creativity and technology is what intrigues me and drives me to persue a career in the 
+                tech industry. 
+            </p>
+            <p>When I'm not busy, you can find me playing chess, traveling, cycling, or spending time with my dogs.</p>
+        </div>
+    )
+}
+
+export default About;
