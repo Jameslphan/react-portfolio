c19aa4f977096664ecc037b24b61c55d2de18241
Phanimal <jameslphan@hotmail.com>
2022-12-28 06:05:24 -0800

    contact form

---
+++ react-portfolio/src/components/Resume/index.js
@@ -0,0 +1,10 @@
+import './index.scss';
+
+const Resume = () => {
+    return(
+        <div>
+        </div>
+    )
+}
+
+export default Resume;
--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -3,7 +3,7 @@ import './index.scss';
 import { Link, NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import reactLogo from '../../assets/images/logo192.png';
-import { faUserSecret, faAddressCard, faChessRook, faSuitcase } from '@fortawesome/free-solid-svg-icons';
+import { faUserSecret, faAddressCard, faChessRook, faSuitcase, faFile } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => {
@@ -23,6 +23,10 @@ const Sidebar = () => {
 
                 <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio" >
                     <FontAwesomeIcon icon={ faSuitcase} color="#4d4d4e" />                   
+                </NavLink>
+
+                <NavLink exact="true" activeclassname="active" className="resume-link" to="/resume" >
+                    <FontAwesomeIcon icon={ faFile} color="#4d4d4e" />                   
                 </NavLink>
 
                 <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact" >
